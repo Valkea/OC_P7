f2655864f1880b4f5c618790f8a6adab8b848584
Valkea <github-sub@shedge.com>
2021-01-26 23:53:53 +0100

    fix: correct imprecision problems which led to share selection whose cost was more than the capacity
    feat: improve the data cleaning process so instead of parsing 100000 rows we now parse 38833

--- OPTI__two_years_best_invest.py
+++ OPTI__two_years_best_invest.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-from math import ceil
 import time
 import logging as lg
 
 import pandas as pd
 import numpy as np
+import psutil
 
 # import psutils
 
@@ -15,7 +15,7 @@ from utils import ProgressBar
 progress_monitor = ProgressBar()
 
 lg.basicConfig(filename="algo.log", filemode="w", level=lg.DEBUG)
-lg.disable(lg.DEBUG)
+# lg.disable(lg.DEBUG)
 
 
 def main(file_name):
@@ -29,6 +29,9 @@ def main(file_name):
 
     start_t = time.time()
 
+    capacity = 500
+    unbounded = True
+
     # --- LOAD data ---
 
     col_names = ["Shares", "Cost(Euro/share)", "Profit(% post 2 years)"]
@@ -37,16 +40,30 @@ def main(file_name):
 
     # --- CLEAN data ---
 
+    # remove invalid data
     df = df.replace([np.inf, -np.inf, 0.0], np.nan).dropna(axis=0)
+
+    # remove SHARES whos value is greater than the capacity
+    df = df[df[col_names[1]] <= capacity]
+
+    # remove extra entries with the very same cost and profit
+    df.drop_duplicates(subset=[col_names[1], col_names[2]], keep="last", inplace=True)
+
+    # df2 = df[df[col_names[1]] == 16.5]
+    # lg.debug(f"DF2: {df2}")
+
+    # --- ADD computed data ---
+
+    # df["ratio"] = df[col_names[2]] / df[col_names[1]]
+    df["gain"] = df[col_names[1]] / 100.0 * df[col_names[2]]
+
     lg.debug(df)
 
     # --- PREPARE data for the algorithm ---
 
     names = [x for x in df[col_names[0]]]
     costs = [x for x in df[col_names[1]]]
-    profits = [x for x in df[col_names[2]]]
-    capacity = 500
-    unbounded = True
+    profits = [x for x in df["gain"]]
 
     # --- CALL the algorithm ----
 
@@ -56,20 +73,30 @@ def main(file_name):
 
     # --- PARSE the returned values ---
 
-    results = {}
-    for share_index in share_indexes:
-        results[names[share_index]] = share_indexes.count(share_index)
+    if share_indexes:
 
-    print("\n\n")
+        results = {}
+        for share_index in share_indexes:
+            results[names[share_index]] = (
+                share_indexes.count(share_index),
+                costs[share_index],
+            )
 
-    print(f"The maximum profit is {profit} \n\nwith the following shares:\n")
-    for k, v in results.items():
-        print(f"- {k} x {v}")
+        print("\n" * 4, " TOP 1 ".center(50, "*"), sep="", end="\n\n")
+
+        print(f"The maximum profit is {profit:.2f}€ \n\nwith the following shares:\n")
+        for k, v in results.items():
+            print(f"- {k} [{v[1]}€] x {v[0]}")
+
+        total = sum([v[0] * v[1] for k, v in results.items()])
+        print(f"\nTotal: {total:.2f}€")
+    else:
+        print("\nNO MATCH FOUND")
 
     # --- CLOSE ---
 
     end_t = time.time()
-    print(f"\nTime: {end_t-start_t} seconds")
+    print("", "*" * 50, f"\nTime: {end_t-start_t} seconds", sep="\n", end="\n" * 2)
 
 
 def knapsack_dynamic_programming(costs, profits, capacity, names=[], unbounded=False):
@@ -96,20 +123,20 @@ def knapsack_dynamic_programming(costs, profits, capacity, names=[], unbounded=F
         The list of the indexes of the actions selected to reach the optimal profit
     """
 
-    step_size = min(costs)
-    # if step_size < 1:  # TODO
-    #    step_size = 1
+    step_size = 0.1
 
     # --- INITIALIZE the grids / matrix ---
 
     rows = len(costs)
-    columns = ceil(capacity / step_size)
+    columns = round(capacity / step_size)
     lg.debug(f"step_size:{step_size}, rows:{rows}, columns:{columns}")
 
+    # grid_costs = np.zeros([rows, columns])
     grid_values = np.zeros([rows, columns])
     grid_shares = [[None for x in range(columns)] for y in range(rows)]
 
     # --- RUN the algorithm ---
+    lg.info(f"PSUTIL BEFORE: {psutil.virtual_memory()}")
 
     # for each provided action
     for i in range(rows):
@@ -120,39 +147,44 @@ def knapsack_dynamic_programming(costs, profits, capacity, names=[], unbounded=F
         if unbounded:
             ref_row = i
 
+        precision_scale = 100
+
         # for each sub-sack in the grid
         for j in range(columns):
 
             step_value = (j + 1) * step_size
-            lg.debug(f"step_value:{step_value}")
+            # lg.debug(f"step_value:{step_value}")
 
             # if current sub-sack can hold at least one share
             if step_value >= costs[i]:
-                lg.debug(f"STEP_VALUE({step_value}) >= COSTS[{i}]({costs[i]})")
+                # lg.debug(f"STEP_VALUE({step_value}) >= COSTS[{i}]({costs[i]}) ==> profit:{profits[i]*precision_scale}")
 
                 # Get the previous max profit for this sub-capacity
                 prev_max = grid_values[i - 1][j] if i > 0 else 0
 
                 # Get the current max profit for this sub-capacity
-                current_value = profits[i]
+                current_value = profits[i] * precision_scale
                 current_max = current_value
 
-                filling_index = j - int(costs[i] // step_size)
+                unused = step_value - costs[i]
+                # lg.debug(f'UNUSED: {unused}')
+                filling_index = round(unused / step_size) - 1
+                # lg.debug(f'FILLING INDEX: {filling_index}')
                 current_max += (
                     grid_values[ref_row][filling_index] if filling_index > -1 else 0
                 )
 
-                lg.debug(f"prev_max:{prev_max}")
-                lg.debug(
-                    f"curr_max:{current_max}, current_value:{current_value}, filling_index:{filling_index}"
-                )
+                # lg.debug(f"prev_max:{prev_max}")
+                # lg.debug(
+                #      f"curr_max:{current_max}, current_value:{current_value}, filling_index:{filling_index}"
+                # )
 
-                # Set the new max profit
+                # Save the new max profit
                 new_max = max(prev_max, current_max)
                 grid_values[i][j] = new_max
-                # lg.debug(f"GRID VALUES: {grid_values}")
+                # lg.debug(f"GRID VALUES: \n{grid_values}")
 
-                # Set the corresponding shares
+                # Save the corresponding shares & costs
                 if new_max == prev_max:
                     grid_shares[i][j] = grid_shares[i - 1][j]
                 else:
@@ -166,13 +198,19 @@ def knapsack_dynamic_programming(costs, profits, capacity, names=[], unbounded=F
 
             # if current sub-sack CAN'T hold at least one share
             else:
-                lg.debug(f"STEP_VALUE({step_value}) < COSTS[{i}]({costs[i]})")
+                # lg.debug(f"STEP_VALUE({step_value}) < COSTS[{i}]({costs[i]})")
                 grid_values[i][j] = grid_values[i - 1][j] if i > 0 else 0
                 grid_shares[i][j] = grid_shares[i - 1][j] if i > 0 else None
 
+            # lg.info(f"PSUTIL IN LOOP J: {psutil.virtual_memory()}")
+
     # --- RETURN values ---
-    progress_monitor.update(i + 1, rows, "Finished")
-    return grid_values[i][j], grid_shares[i][j]
+    # for row in grid_values:
+    #     lg.debug(f"GRID ROW VALUES: {row}")
+    lg.info(f"PSUTIL AFTER: {psutil.virtual_memory()}")
+
+    progress_monitor.update(i + 1, rows, "Finished OPTIMIZED search")
+    return grid_values[i][j] / precision_scale, grid_shares[i][j]
 
 
 if __name__ == "__main__":
